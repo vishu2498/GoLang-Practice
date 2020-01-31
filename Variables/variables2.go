02fb8dde9481d3db1ed05da1c61661a82292128d
Vishwanath Taykhande <54012750+Vishwanath-GitHub@users.noreply.github.com>
2020-01-31 13:04:34 +0530

    Add files via upload

--- Variables/variables2.go
+++ Variables/variables2.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"strconv"
+)
 func main() {
-	
+	//Type Casting
+	var var13 float64 = 51.32
+	var var14 int = int(var13) //information can also be lost during type casting/conversion
+	fmt.Printf("%v, %T",var14,var14)
+	fmt.Println()
+
+	var15:=13
+	var var16 string = strconv.Itoa(var15) //'Itoa' means integer to ASCII
+	fmt.Println(var16)
+
 }
