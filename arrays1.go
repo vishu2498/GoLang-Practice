7e004c6e6bf63acfae8992f8ae5491d3d5508109
Vishwanath Taykhande <54012750+Vishwanath-GitHub@users.noreply.github.com>
2020-02-01 18:26:24 +0530

    Add files via upload

---
+++ arrays1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Arrays are declared by mentioning the size first (in the brackets) and then declaring its data-type
+	arr1:=[3]int{51,94,37} //Initializer Syntax
+	fmt.Println(arr1)
+
+	//Different way of defining values of an array by Initializer syntax
+	arr6:=[3]int{0:27,1:56, 2:98}
+	fmt.Println(arr6)
+
+	//If we don't want to declare the size of the array and just define the values and let GoLang compiler know the size of array itself:
+	arr2:=[...]string{"hello","there","you"} //'...' literal tells GoLang compiler to know the size of array itself
+	fmt.Println(arr2)
+
+	//Using array with size defined but no values provided
+	var arr3 [4]string
+	fmt.Println(arr3)
+
+	//Defining values of array after declaring it
+	//Values of array can be defined in any order
+	var arr4 [4]float64
+	arr4[0]=25.31
+	arr4[1]=98.24
+	arr4[2]=8.59
+	arr4[3]=9.58
+	fmt.Println(arr4)
+	fmt.Println(arr4[2]) //getting 3rd element of array
+
+	//Getting the length of the array
+	fmt.Println("Length of array: ",len(arr4))
+
+	//Getting the capacity of array
+	var arr5 [6]string
+	fmt.Println("Capacity of array: ",cap(arr5))
+}
---
+++ arrays2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Two-Dimensional Array
+	arr1:=[3][3]int{[3]int{1,2,5}, [3]int{5,6,1}, [3]int{4,9,7}}
+	fmt.Println(arr1)
+
+	//Refined way of declaring and defining two-dimenstional array
+	var arr2 [3][3]int
+	arr2[0]=[3]int{4,6,1}
+	arr2[1]=[3]int{4,6,1}
+	arr2[2]=[3]int{4,6,1}
+	fmt.Println(arr2)
+	fmt.Println()
+
+	//If an array is defined equal to another array, it doesn't point to that array. Rather it creates copy of it and saves it inside itself
+	arr3:=[3]int{2,5,4}
+	arr4:=arr3
+	arr4[1]=8
+	fmt.Println(arr3) //Output shows that even if the value is changed in arr4, the original arr3 array has no effect of it
+	fmt.Println(arr4) //It will show the changed array
+
+	//To make an array directly point to another array, we need to make use of '&' operator
+
+	arr5:=[3]int{2,4,9}
+	arr6:=&arr5
+	arr6[1]=5
+	fmt.Println(arr5) //Output shows that the value of arr5 is changed too
+	fmt.Println(arr6) //Output shows that arr6 is now actually pointing to arr5
+}
