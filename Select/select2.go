473e7d45457ce452a8cb699eadf6330d38b99e5f
Vishwanath Taykhande <vishwanath.taykhande@CALSOFTHQ.COM>
2020-02-10 16:12:36 +0530

    default case

--- Select/select2.go
+++ Select/select2.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	result1:=make(chan string)
+	result2:=make(chan string)
+
+	go printmsg1(result1)
+	go printmsg2(result2)
+
+	select {
+	case v1:=<-result1:
+		fmt.Println(v1)
+	case v2:=<-result2:
+		fmt.Println(v2)
+	//'default' keyword is used when any of the channels are blocking the code and no other cases are working.
+	//Here, since it will take longer than the wait time of GoLang to get the data from channel, 'default' case will execute.
+	//This is generally used to prevent the 'select' statement from blocking.
+	default:
+		fmt.Println("took too long to respond")
+	//'default' also the prevents the program moving to deadlock state. So, if the select cases are blocking the code and the program is entering deadlock state, 'default' will prevent that.
+	}
+}
+
+func printmsg1(ch chan string) {
+	time.Sleep(10*time.Second)
+	ch<-"1st"
+}
+
+func printmsg2(ch chan string)  {
+	time.Sleep(5*time.Second)
+	ch<-"2nd"
+}
