842173740d62d870f82aa2f180c101715b0500be
Vishwanath Taykhande <vishwanath.taykhande@CALSOFTHQ.COM>
2020-02-11 16:34:52 +0530

    nil channel

---
+++ Channels/channel13.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Nil-channel is that channel that it explicitly given nil value.
+	//So, the channel will always be in blocked state and passing any value to it will result in program deadlock.
+	var ch chan string
+	ch = nil
+	ch <- "hello"
+	fmt.Printf("%v, %T", ch, ch)
+}
+
