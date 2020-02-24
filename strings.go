21549d77a7b67a48992276a70cf23a48926f56aa
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-02-24 15:09:22 +0530

    character implementation

--- strings.go
+++ strings.go
@@ -29,4 +29,24 @@ func main() {
 	var6:="egstring"
 	fmt.Println(strings.Trim(var6,"eg"))
 	fmt.Println(var6) //showing that 'strings.Trim()' works on copy of data passed into arguments and not on original data
+	fmt.Println()
+	
+	//Characters defined explicitly are actually of 'int32' data-type.
+	var7:='v'
+	fmt.Printf("%v, %T", var7,var7) //output shows that when a character is printed directly, its ASCII value is shown since it is of 'int32' data-type
+	fmt.Println()
+	//To get the string representation of any character, '%c' operator is used.
+	fmt.Printf("%c",var7)
+	fmt.Println()
+	//It can also be type-casted to string.
+	fmt.Println(string(var7))
+	//Since character is of 'int32' data-type integers values can be operated on it to get another simultaneous character in alphabets.
+	fmt.Println('h'+2) //will only show the ASCII value
+	fmt.Println(string('h'+2))
+	fmt.Println()
+
+	//Printing all alphabets from character
+	for var9:='a';var9<'a'+26 ; var9++ {
+		fmt.Print(string(var9))
+	}
 }
