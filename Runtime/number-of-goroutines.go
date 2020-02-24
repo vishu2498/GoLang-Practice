071c3c41ae129cac3e72d0c0ecd9aac8265a8674
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-02-24 12:09:02 +0530

    getting number of goroutines currently running

--- Runtime/number-of-goroutines.go
+++ Runtime/number-of-goroutines.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	wg.Add(1)
+	go func() {
+		fmt.Println("This will print afterwards")
+		wg.Done()
+	}()
+	//NumGoroutine returns the number of goroutines that currently exist.
+	fmt.Println(runtime.NumGoroutine())
+	wg.Wait()
+	fmt.Println(runtime.NumGoroutine()) //However, if it used after execution of a goroutine is done, it will not count that goroutine.
+}
