102cedb3ae598492a631392e80adf37cf9582c6f
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-02-28 14:41:47 +0530

    log panic

--- Log/log2.go
+++ Log/log2.go
@@ -1,1 +1,9 @@
 package main
+import "log"
+
+func main() {
+	log.Panic("log panic") //will create a panic with the given string as a log
+	//This function will exit the program with exit code 2.
+	//'log.Panicf()' and 'log.Panicln()' are also present.
+	//If the panic is not recovered, then the program will terminate with a stack trace.
+}
