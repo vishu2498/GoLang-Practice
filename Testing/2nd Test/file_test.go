7642931a69fa71be041f013c22a469c4fc30f1e5
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-03 17:53:27 +0530

    resuming execution when test-case fails

--- Testing/2nd Test/file_test.go
+++ Testing/2nd Test/file_test.go
@@ -1,1 +1,21 @@
-package _nd_Test
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	if f1()==nil {
+		t.Fatal("received error")
+	} else {
+		t.Error("did not receive an error") //works similar to 'Log()' but uses function 'FailNow()' that reports that test-case is failed.
+		//this is particularly used when test-case is failed but program should not stop execution.
+		//'Errorf()' is also available.
+	}
+}
+
+func f1() error {
+	var1 := errors.New("this is an error")
+	return var1
+}
