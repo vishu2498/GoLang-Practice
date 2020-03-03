c7bec18882beee9021c75cc6582e6bf78d60a77e
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-03 12:59:48 +0530

    math.Max() and comments added

--- Short Programs/find-max.go
+++ Short Programs/find-max.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	values := []int{1, 5, 92, 104, 21}
 	fmt.Println(max(values))
+	fmt.Println()
+
+	//'math.Max()' can also be used to find bigger number (accepts float numbers)
+	//But it can only take two values
+	fmt.Println(math.Max(65,212))
 }
 
 func max(values []int) int {
 	max := values[0]
-	for i := 0; i < len(values); i++ {
-		if values[i] > max {
+	for i := 0; i < len(values); i++ { //'i' can be both '0' or '1'
+		if values[i] > max { //Don't get confused that values[i] is the index. It is the value of that index.
 			max = values[i]
 		}
 	}
