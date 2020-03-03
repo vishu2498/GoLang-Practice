94e4dc5468a49cc3c4a0973feb6b32fc984b8a38
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-03 18:09:18 +0530

    simple addition test-case with values provided from test-case

--- Testing/Simple Addition Test-Case/add_test.go
+++ Testing/Simple Addition Test-Case/add_test.go
@@ -3,15 +3,15 @@ package main
 import "testing"
 
 func Test(t *testing.T) {
-	if f1() == 30 {
+	value:=f1(10,20)
+	if value == 30 {
 		t.Log("value is correct")
 	} else {
 		t.Error("value is incorrect")
 	}
 }
 
-func f1() int {
-	var1, var2 := 10, 20
-	var3 := var1 + var2
-	return var3
+func f1(value1, value2 int) int {
+	result := value1 + value2
+	return result
 }
