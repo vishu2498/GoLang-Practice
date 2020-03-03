56dfb1a29141815f8f969c720bc3cf442e3444d6
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-03 11:09:34 +0530

    ASCII to Integer

--- strings.go
+++ strings.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -17,16 +18,16 @@ func main() {
 
 	//Iterating over a string
 	var var15 string
-	var15="vishu"
-	for i:=0; i<len(var15); i++ {
+	var15 = "vishu"
+	for i := 0; i < len(var15); i++ {
 		fmt.Print(string(var15[i]))
 	}
 	fmt.Println()
 
 	//Reverse-Iterating over a string
 	var var14 string
-	var14="vishu"
-	for i:=len(var14)-1; i>=0; i-- {
+	var14 = "vishu"
+	for i := len(var14) - 1; i >= 0; i-- {
 		fmt.Print(string(var14[i]))
 	}
 	fmt.Println()
@@ -34,13 +35,13 @@ func main() {
 	//Iterating over the string since the inside representation of a string is byte
 	//By for-range loop
 	var var10 string
-	var10="hello"
+	var10 = "hello"
 	for key, value := range var10 {
-		fmt.Println(key,value) //will print the the index of each character of string with its ASCII value
+		fmt.Println(key, value) //will print the the index of each character of string with its ASCII value
 	}
 	fmt.Println()
 	for key, value := range var10 {
-		fmt.Println(key,string(value)) //will print the the index of each character of string with its string character
+		fmt.Println(key, string(value)) //will print the the index of each character of string with its string character
 	}
 	fmt.Println()
 
@@ -55,27 +56,27 @@ func main() {
 
 	//'string.Trim()' from 'strings' package cuts the string given in the 2nd argument from the 1st argument
 	//And returns the remaining slice of string
-	var6:="egstring"
-	fmt.Println(strings.Trim(var6,"eg"))
+	var6 := "egstring"
+	fmt.Println(strings.Trim(var6, "eg"))
 	fmt.Println(var6) //showing that 'strings.Trim()' works on copy of data passed into arguments and not on original data
 	fmt.Println()
-	
+
 	//Characters defined explicitly are actually of 'int32' data-type.
-	var7:='v'
-	fmt.Printf("%v, %T", var7,var7) //output shows that when a character is printed directly, its ASCII value is shown since it is of 'int32' data-type
+	var7 := 'v'
+	fmt.Printf("%v, %T", var7, var7) //output shows that when a character is printed directly, its ASCII value is shown since it is of 'int32' data-type
 	fmt.Println()
 	//To get the string representation of any character, '%c' operator is used.
-	fmt.Printf("%c",var7)
+	fmt.Printf("%c", var7)
 	fmt.Println()
 	//It can also be type-casted to string.
 	fmt.Println(string(var7))
 	//Since character is of 'int32' data-type integers values can be operated on it to get another simultaneous character in alphabets.
-	fmt.Println('h'+2) //will only show the ASCII value
-	fmt.Println(string('h'+2))
+	fmt.Println('h' + 2) //will only show the ASCII value
+	fmt.Println(string('h' + 2))
 	fmt.Println()
 
 	//Printing all alphabets from character
-	for var9:='a';var9<'a'+26 ; var9++ {
+	for var9 := 'a'; var9 < 'a'+26; var9++ {
 		fmt.Print(string(var9))
 	}
 	fmt.Println()
@@ -85,12 +86,20 @@ func main() {
 	//It returns '0' if strings are equal.
 	//It returns '-1' if 1st string is bigger than 2nd string or both strings are unequal.
 	//It returns '1' if 2nd string is bigger than 1st string.
-	var11:="hello"
-	var12:="vishu"
-	var13:=strings.Compare(var11,var12)
-	if var13==0 {
+	var11 := "hello"
+	var12 := "vishu"
+	var13 := strings.Compare(var11, var12)
+	if var13 == 0 {
 		fmt.Println(true)
 	} else {
 		fmt.Println(false)
+	}
+
+	//Checking if the given string is actually a number
+	//'strconv.Atoi()' converts ASCII values of the argument to integer and returns the integer.
+	//If integer is not returned, we get an error.
+	var16 := "7"
+	if _, err := strconv.Atoi(var16); err == nil {
+		fmt.Printf("%q looks like a number.\n", var16) //%q is used to apply double quotes with the given object
 	}
 }
