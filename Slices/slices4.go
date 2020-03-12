4306055447f23bc717adbc48b7a983e40f0d8050
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-12 18:13:59 +0530

    Making a slice with 1000 elements and adding them with implementation of GoRoutine

--- Slices/slices4.go
+++ Slices/slices4.go
@@ -1,1 +1,26 @@
+//Making a slice with 1000 elements and adding them with implementation of GoRoutine
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	slice1 := make([]int, 1000)
+	wg.Add(1)
+	go f1(slice1)
+	wg.Wait()
+}
+
+func f1(slice []int) (int, error) {
+	var sum int
+	for _, values := range slice {
+		sum = sum + values
+	}
+	wg.Done()
+	return fmt.Println(sum)
+}
+
