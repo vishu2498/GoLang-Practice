a9becc3ab042995dd7e8a057bdd8008d1b79387c
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-16 12:10:45 +0530

    adding more functions with usage of SetupTest()

--- Testing/Testify/suitepkg/src/suite1_test.go
+++ Testing/Testify/suitepkg/src/suite1_test.go
@@ -18,10 +18,29 @@ func TestStackSuite(t *testing.T) {
 //This function runs before each test and is called by testify.
 //It can also be regarded as a configuration for all the test (like a permanent receiver for all tests).
 func (s *StackSuite) SetupTest() {
-	//Instead of using 'stack:= suitepkg.NewStack()' in every test, we can configure it once here.
+	//Instead of using 'stack:= suitepkg1.NewStack()' in every test, we can configure it once here.
 	s.stack=suitepkg1.NewStack()
 }
 
 func (s *StackSuite) TestEmpty() {
+	s.True(s.stack.IsEmpty())
+}
+
+func (s *StackSuite) TestUnburySingleItem() {
+	s.stack.Bury("green")
+	s.Equal("green",s.stack.Unbury())
+	s.Zero(s.stack.Size())
+	s.True(s.stack.IsEmpty())
+}
+
+func (s *StackSuite) TestUnburyMultipleItems() {
+	s.stack.Bury("green")
+	s.stack.Bury("yellow")
+
+	s.Equal("yellow",s.stack.Unbury())
+	s.Equal(1,s.stack.Size())
+
+	s.Equal("green",s.stack.Unbury()) //If we don't 'Unbury()' the times 'Bury()' was used, test will fail.
+	s.Zero(s.stack.Size())
 	s.True(s.stack.IsEmpty())
 }
--- Testing/Testify/suitepkg/src/suitepkg1/suite1.go
+++ Testing/Testify/suitepkg/src/suitepkg1/suite1.go
@@ -3,6 +3,7 @@ package suitepkg1
 type Stack struct {
 	isEmpty bool
 	size int
+	items [6]string
 }
 
 func (stack *Stack) IsEmpty() bool {
@@ -10,12 +11,18 @@ func (stack *Stack) IsEmpty() bool {
 }
 
 func (stack *Stack) Bury(item string) {
+	stack.items[stack.size]=item
 	stack.size++
 }
 
 func (stack *Stack) Size() int {
 	return stack.size
 }
+
+func (stack *Stack) Unbury() string {
+	stack.size--
+	return stack.items[stack.size]
+}
 func NewStack() *Stack {
-	return &Stack{true,3}
+	return &Stack{true,0,[6]string{}}
 }
