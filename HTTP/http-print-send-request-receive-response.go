0a58531cc93454b671ac600c15a6c6cdbe5f1a9b
Vishwanath Taykhande <vishwanath.taykhande@calsofthq.com>
2020-03-19 15:29:18 +0530

    adding hit-counter

--- HTTP/http-print-send-request-receive-response.go
+++ HTTP/http-print-send-request-receive-response.go
@@ -6,14 +6,17 @@ import (
 	"net/http"
 )
 
+var HitCounter int
+
 func main() {
 	http.HandleFunc("/users", usersHandleFunc) //it registers the handler function for working of HTTP
 	log.Fatal(http.ListenAndServe(":8080", nil)) //starts, displays and listens on the server
 }
 
 func usersHandleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received a request.") //will be displayed on the console/terminal
-	fmt.Fprintf(w, "Hello there. This was received using method '%v'", r.Method)
+	HitCounter++
+	fmt.Printf("Received a request '%v' times.\n",HitCounter) //will be displayed on the console/terminal
+	fmt.Fprintf(w, "Hello there. This was received using method '%v' and you called this page '%v' times.", r.Method,HitCounter)
 }
 
 //Run this program and visit "http://localhost:8080/users" on browser.
